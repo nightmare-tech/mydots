53931517883d367450dacf85700e49753f9b3131
Arpit Gupta <arpitguptaa004@gmail.com>
2024-08-15 17:11:13 +0530

    Added delay

--- scripts/powercycle.sh
+++ scripts/powercycle.sh
@@ -5,17 +5,17 @@ case "$var" in
     *balanced*)
         echo "Executing command for balanced profile"
         powerprofilesctl set performance
-        notify-send "Performance" -a "PowerCycle"
+        notify-send -t 600 "Performance" -a "PowerCycle"
         ;;
     *power-saver*)
         echo "Executing command for power-saver profile"
         powerprofilesctl set balanced
-        notify-send "Balanced" -a "PowerCycle"
+        notify-send -t 600"Balanced" -a "PowerCycle"
         ;;
     *performance*)
         echo "Executing command for performance profile"
         powerprofilesctl set power-saver
-        notify-send "Power Saver" -a "PowerCycle"  
+        notify-send -t 600 "Power Saver" -a "PowerCycle"  
         ;;
     *)
         echo "Error: Unknown power profile"
